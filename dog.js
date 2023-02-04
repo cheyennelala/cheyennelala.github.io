5c55247f2d9c85b21ebcb1ecb5cd14ef2586f4df
cheyennelala <117870675+cheyennelala@users.noreply.github.com>
2023-02-04 15:12:31 -0500

    Add files via upload

---
+++ dog.js
@@ -0,0 +1,13 @@
+let myDog = {
+    name: "Scooby-Doo",
+    breed: "Great Dane",
+    owner: "Mystery Inc.",
+    traits: ["Brave", "Hungry", "Friendly"],
+    mySound: "Ruh-roh, Raggy!",
+
+}
+
+
+alert(`Meet ${myDog.name}, a ${myDog.breed} who is owned by ${myDog.owner}. ${myDog.name} is known for being ${myDog.traits[0]}, ${myDog.traits[1]}, and ${myDog.traits[2]}. When you hear ${myDog.name} bark, ${myDog.name} might say "${myDog.mySound}"`);
+
+
